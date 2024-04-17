f3499bca281c8714f6f1557999581349c0d295a8
Vinoth Kumar <52799229+vinothkumar-6@users.noreply.github.com>
2024-04-17 15:00:23 -0700

    initial commit

---
+++ currency-exchange-microservice/src/main/java/com/getajob/microservices/currencyexchangeservice/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.getajob.microservices.currencyexchangeservice;
+
+public class HelloWorld {
+
+    public String sayHello(String name) {
+
+        System.out.println("sayHello called with name : " + name);
+        return "Hello " + name;
+    }
+
+}
